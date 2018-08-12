4889b0229f3a838162a3f3b69b85a770120a6637
Gloria Sevilla <40722103+Seglo28@users.noreply.github.com>
2018-08-11 23:07:55 -0600

    Dos

--- BibliotecaConsola/src/bibliotecaconsola/BibliotecaConsola.java
+++ BibliotecaConsola/src/bibliotecaconsola/BibliotecaConsola.java
@@ -16,6 +16,7 @@ public class BibliotecaConsola {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Mi primer hola mundo.");
     }
     
 }
